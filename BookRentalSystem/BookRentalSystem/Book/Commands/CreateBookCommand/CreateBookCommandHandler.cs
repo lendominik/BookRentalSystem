5d736f18dbb5c9774b340a2cf4d66108fbd6f4a8
lendominik <len.dominik13@gmail.com>
2024-12-19 00:12:41 +0100

    Implement Unit Of Work

--- BookRentalSystem/BookRentalSystem/Book/Commands/CreateBookCommand/CreateBookCommandHandler.cs
+++ BookRentalSystem/BookRentalSystem/Book/Commands/CreateBookCommand/CreateBookCommandHandler.cs
@@ -6,27 +6,24 @@ using MediatR;
 namespace BookRentalSystem.Book.Commands.CreateBookCommand;
 
 public class CreateBookCommandHandler(
-    IGenericRepository<Core.Entities.Book> repository,
-    IGenericRepository<Core.Entities.Author> authorRepository,
-    IGenericRepository<Core.Entities.Publisher> publisherRepository,
-    IGenericRepository<Core.Entities.Category> categoryRepository,
+    IUnitOfWork unitOfWork,
     IMapper mapper)
     : IRequestHandler<CreateBookCommand>
 {
     public async Task Handle(CreateBookCommand request, CancellationToken cancellationToken)
     {
-        var authorExsists = authorRepository.Exists(request.AuthorId);
-        var publisherExsists = publisherRepository.Exists(request.PublisherId);
-        var categoryExsists = categoryRepository.Exists(request.CategoryId);
+        var authorExsists = unitOfWork.Repository<Core.Entities.Author>().Exists(request.AuthorId);
+        var publisherExsists = unitOfWork.Repository<Core.Entities.Publisher>().Exists(request.PublisherId);
+        var categoryExsists = unitOfWork.Repository<Core.Entities.Category>().Exists(request.CategoryId);
 
         if (!authorExsists || !publisherExsists || !categoryExsists)
             throw new NotFoundException("Author, Publisher or Category not found");
 
         var book = mapper.Map<Core.Entities.Book>(request);
 
-        repository.Add(book);
+        unitOfWork.Repository<Core.Entities.Book>().Add(book);
 
-        if (!await repository.SaveAllAsync())
+        if (!await unitOfWork.Repository<Core.Entities.Author>().SaveAllAsync())
             throw new BadRequestException("Problem creating book");
     }
 }
