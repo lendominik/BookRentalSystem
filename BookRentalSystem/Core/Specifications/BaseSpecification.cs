5fd21d3369c431221a09790972269673dc1954ff
lendominik <len.dominik13@gmail.com>
2024-12-18 17:58:09 +0100

    Update specification pattern (Add orderby and orderbydesc)

--- BookRentalSystem/Core/Specifications/BaseSpecification.cs
+++ BookRentalSystem/Core/Specifications/BaseSpecification.cs
@@ -6,4 +6,18 @@ namespace Core.Specifications;
 public class BaseSpecification<T>(Expression<Func<T, bool>> criteria) : ISpecification<T>
 {
     public Expression<Func<T, bool>> Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
 }
