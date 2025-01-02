e1f49fb23050f3c834223b2e3e0a5d4a69276faa
lendominik <len.dominik13@gmail.com>
2025-01-02 22:14:34 +0100

    update program.cs

--- BookRentalSystem/BookRentalSystem/Program.cs
+++ BookRentalSystem/BookRentalSystem/Program.cs
@@ -1,6 +1,15 @@
+using Application.Extensions;
+using BookRentalSystem.Middlewares;
+using Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddExceptionHandler<ExceptionHandler>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
