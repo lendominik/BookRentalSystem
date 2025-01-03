168f3d038be42343428f7f9fd781592a80445aa7
lendominik <len.dominik13@gmail.com>
2025-01-03 17:31:12 +0100

    fixed

---
+++ BookRentalSystem/BookRentalSystem/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Category.Commands.CreateCategoryCommand;
+using Application.Category.Queries.GetCategoryByIdQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookRentalSystem.Controllers;
+
+public class CategoryController(
+    IMediator mediator)
+    : Controller
+{
+    [HttpPost]
+    [Route("api/categories")]
+    public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+    {
+        await mediator.Send(command);
+        return Ok();
+    }
+
+    [Route("api/categories/{categoryId}/Details")]
+    public async Task<IActionResult> Details(int categoryId)
+    {
+        var categoryDto = await mediator.Send(new GetCategoryByIdQuery(categoryId));
+        return View(categoryDto);
+    }
+}
