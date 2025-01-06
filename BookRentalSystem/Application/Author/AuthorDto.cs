f51727145fdb00c25b9f7b3f12cab6f8ef856f7a
lendominik <len.dominik13@gmail.com>
2025-01-06 09:14:40 +0100

    update CreateAuthorCommand

--- BookRentalSystem/Application/Author/AuthorDto.cs
+++ BookRentalSystem/Application/Author/AuthorDto.cs
@@ -5,8 +5,10 @@ namespace Application.Author;
 public class AuthorDto
 {
     public int Id { get; set; }
+    [Required]
     [Display(Name = "First Name")]
     public string FirstName { get; set; } = default!;
+    [Required]
     [Display(Name = "Last Name")]
     public string LastName { get; set; } = default!;
     public string? Description { get; set; }
