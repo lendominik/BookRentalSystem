09e1ceab78fe61a7de7898ab70a4bfdf0f65c493
lendominik <len.dominik13@gmail.com>
2025-01-06 10:03:01 +0100

    Create admin controller

---
+++ BookRentalSystem/BookRentalSystem/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookRentalSystem.Controllers;
+
+public class AdminController(
+    UserManager<IdentityUser> userManager)
+    : Controller
+{
+    public IActionResult Index()
+    {
+        var users = userManager.Users.ToList();
+        return View(users);
+    }
+}
