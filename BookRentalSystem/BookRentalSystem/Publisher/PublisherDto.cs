a2c70bfd132d6259582feb975efde7587a264d99
lendominik <len.dominik13@gmail.com>
2025-01-12 16:53:19 +0100

    fixed

--- BookRentalSystem/BookRentalSystem/Publisher/PublisherDto.cs
+++ BookRentalSystem/BookRentalSystem/Publisher/PublisherDto.cs
@@ -4,6 +4,7 @@ namespace BookRentalSystem.Publisher;
 
 public class PublisherDto : IRequest
 {
+    public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
 }
--- BookRentalSystem/BookRentalSystem/Review/ReviewDto.cs
+++ BookRentalSystem/BookRentalSystem/Review/ReviewDto.cs
@@ -2,6 +2,7 @@
 
 public class ReviewDto
 {
+    public int Id { get; set; }
     public string Content { get; set; } = null!;
     public string ReviewerName { get; set; } = "Anonymous";
     public DateTime CreatedDate { get; set; } = DateTime.Now;
