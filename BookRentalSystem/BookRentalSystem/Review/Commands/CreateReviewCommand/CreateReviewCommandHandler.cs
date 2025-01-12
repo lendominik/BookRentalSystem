4f3f7a0b5522f720a3f761d379f355be0cb5c841
lendominik <len.dominik13@gmail.com>
2025-01-12 21:06:49 +0100

    fixed

--- BookRentalSystem/BookRentalSystem/Review/Commands/CreateReviewCommand/CreateReviewCommandHandler.cs
+++ BookRentalSystem/BookRentalSystem/Review/Commands/CreateReviewCommand/CreateReviewCommandHandler.cs
@@ -6,22 +6,22 @@ using MediatR;
 namespace BookRentalSystem.Review.Commands.CreateReviewCommand;
 
 public class CreateReviewCommandHandler(
-    IGenericRepository<Core.Entities.Review> repository,
+    IUnitOfWork unitOfWork,
     IMapper mapper)
     : IRequestHandler<CreateReviewCommand>
 {
     public async Task Handle(CreateReviewCommand request, CancellationToken cancellationToken)
     {
-        var bookExists = repository.Exists(request.BookId);
+        var bookExists = unitOfWork.Repository<Core.Entities.Book>().Exists(request.BookId);
 
         if (!bookExists) 
             throw new NotFoundException("Book not found");
 
         var review = mapper.Map<Core.Entities.Review>(request);
 
-        repository.Add(review);
+        unitOfWork.Repository<Core.Entities.Review>().Add(review);
 
-        if (!await repository.SaveAllAsync())
+        if (!await unitOfWork.Repository<Core.Entities.Book>().SaveAllAsync())
             throw new BadRequestException("Problem creating review");
     }
 }
